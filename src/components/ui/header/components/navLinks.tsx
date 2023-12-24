69b0d722db89a0f993500b580bc2ca019c29aaff
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-23 23:22:56 -0300

    feat:create navlinks for navbar component

---
+++ src/components/ui/header/components/navLinks.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import styles from "@/components/ui/header/page.module.scss";
+const links = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "sobre",
+    path: "/sobre"
+  },
+  {
+    name: "mods",
+    path: "/mods"
+  },
+  {
+    name: "liquidos",
+    path: "/liquidos"
+  },
+  {
+    name: "equipes",
+    path: "/equipes"
+  },
+  {
+    name: "contato",
+    path: "/contato"
+  }
+];
+
+const NavLinks = () => {
+  return (
+    <nav className={styles.nav_links}>
+      <ul>
+        {links.map(({ name, path }, index) => {
+          return (
+            <li key={index}>
+              <Link href={path}>{name.toUpperCase()}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavLinks;
